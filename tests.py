913bd405493029f28b876ab7c99b0c5b9dc04e85
Sander van Rijn <s.vanrijn@esciencecenter.nl>
2023-04-25 00:07:35 +0200

    fixed stash stuff

--- tests.py
+++ tests.py
@@ -3,12 +3,9 @@ def test_1():
     
 def test_2():
     assert 1+1 == 2
-<<<<<<< Updated upstream
-=======
 
 def test_3():
     assert (1 == 2) is False
 
 def test_4():
     assert []
->>>>>>> Stashed changes
