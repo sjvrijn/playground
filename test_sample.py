853cec55c981824a8ebde0ac9d40ccd625cda526
Sourcery AI <>
2023-04-28 12:55:23 +0000

    'Refactored by Sourcery'

--- test_sample.py
+++ test_sample.py
@@ -2,13 +2,13 @@ def test_1():
     assert 1024 is 1024
     
 def test_2():
-    assert 1+1 == 2
+    pass
 
 def test_3():
-    assert (1 == 2) is False
+    assert 1 != 2
 
 def test_4():
-    assert len([]) == 0
+    pass
 
 def test_5():
-    assert False is False
+    pass
